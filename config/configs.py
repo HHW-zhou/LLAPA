5fe5550b66fefba3abeb2d3974325c91a826fff2
HHW-zhou <31394571+HHW-zhou@users.noreply.github.com>
2025-09-15 12:36:02 +0800

    Update configs.py

--- config/configs.py
+++ config/configs.py
@@ -5,10 +5,6 @@ ROOT_DIR = 'YOUR WORK PATH'
 DATA_DIR = 'YOUR DATASET PATH'
 LLM_CONFIG_PATH = os.path.join(ROOT_DIR, 'LLAPA/config/llama3-3b-backbone')
 
-ROOT_DIR = '/mnt/ai4s_ceph_share/neogaryzhou'
-DATA_DIR = '/mnt/ai4s_ceph_share/neogaryzhou/LLaPA_datasets'
-LLM_CONFIG_PATH = os.path.join(ROOT_DIR, 'lla-pa3/config/llama3-3b-backbone')
-
 WORKERS = 100
 MAX_SEQ_LEN = 1024
 MIN_SEQ_LEN = 10
